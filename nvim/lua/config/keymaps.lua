abb531f1d126d223bea6f5cdc84e174c74ee7a53
Chethin Weerakkody <chethiche123@gmail.com>
2025-12-05 03:59:57 +1100

    moving lines up down in vim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,6 +8,12 @@ map("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 map("n", "<D-j>", "10jzz", { noremap = true, silent = true })
 map("n", "<D-k>", "10kzz", { noremap = true, silent = true })
 
+-- Move lines vertically
+map("n", "<a-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+map("n", "<a-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+map("v", "<a-j>", ":m '>+1<CR>==gv=gv", { noremap = true, silent = true })
+map("v", "<a-k>", ":m '<-2<CR>==gv=gv", { noremap = true, silent = true })
+
 vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- surround
